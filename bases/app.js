32425b18684aadb98cea5b23c0baf9886b8d75e1
Jimmy Salazar <jimmyr.salazar@gmail.com>
2024-01-05 01:31:52 -0500

    first commit

---
+++ bases/app.js
@@ -0,0 +1,8 @@
+"use strict";
+const msg = 'Hola Mundo';
+const hero = {
+    name: 'Ironman',
+    age: 45
+};
+//hero.age = '50'; // TS detecta el error
+console.log(hero.age + 1);
---
+++ bases/app.ts
@@ -0,0 +1,11 @@
+
+const msg: string = 'Hola Mundo';
+
+const hero = {
+    name: 'Ironman',
+    age: 45
+}
+
+//hero.age = '50'; // TS detecta el error
+
+console.log(hero.age + 1);
